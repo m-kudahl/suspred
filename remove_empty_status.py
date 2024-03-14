7b451a1147b56843fa938ee91cf8cf88c3651c0f
Veteranpit <115033980+Veteranpit@users.noreply.github.com>
2024-03-14 10:58:45 +0100

    combined_test_train

--- remove_empty_status.py
+++ remove_empty_status.py
@@ -1,10 +1,21 @@
+
 import pandas as pd
 
-# Read the CSV file
-df = pd.read_csv('combined_data.csv')
+# Read CSV files into DataFrames
+file1_df = pd.read_csv('Filtered_No_Guac.csv')
+file2_df = pd.read_csv('graduation.csv')
+
+# Drop all columns from file2_df except 'name' and 'status'
+file2_df = file2_df[['name', 'status']]
+
+# Merge DataFrames based on the common column
+merged_df = pd.merge(file1_df, file2_df, how='left', left_on='project', right_on='name')
+
+# Update status column in file1_df with the values from file2_df
+file1_df['status'] = merged_df['status']
 
 # Remove rows with empty values in the specified columns
-df.dropna(subset=['status'], inplace=True)
+file1_df.dropna(subset=['status'], inplace=True)
 
 # Write the cleaned dataframe to a new CSV file
-df.to_csv('cleaned_combined_data.csv', index=False)
+file1_df.to_csv('cleaned_combined_data.csv', index=False)
